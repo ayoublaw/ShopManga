751714548ba0f273920cb752c10aa1fe512fb522
Ayoub mouhri <Ayoublaw123@gmail.com>
2019-03-16 14:13:33 +0100

    Search view

--- app/src/main/java/com/android/shopmanga/MainActivity.java
+++ app/src/main/java/com/android/shopmanga/MainActivity.java
@@ -2,6 +2,8 @@ package com.android.shopmanga;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Spinner_List, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
