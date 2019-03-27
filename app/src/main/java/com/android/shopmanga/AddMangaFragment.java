832d3d8bfe801e3defa8e6ae11a4af231b2d4e47
Ayoub Mouhri <ayoublaw123@gmail.com>
2019-03-27 13:19:11 +0100

    Add autocompletaion and currentPosition

--- app/src/main/java/com/android/shopmanga/AddMangaFragment.java
+++ app/src/main/java/com/android/shopmanga/AddMangaFragment.java
@@ -1,8 +1,15 @@
 package com.android.shopmanga;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,16 +25,32 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 import es.dmoral.toasty.Toasty;
 
 
 public class AddMangaFragment extends Fragment {
+
+    private FusedLocationProviderClient client;
+    String address;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,8 +62,60 @@ public class AddMangaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_manga, container, false);
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
 
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getChildFragmentManager().findFragmentById(R.id.AddManga_autocomplete_fragment);
+        EditText text = autocompleteFragment.getView().findViewById(R.id.places_autocomplete_search_input);
+        autocompleteFragment.getView().findViewById(R.id.places_autocomplete_search_button).setVisibility(View.GONE);
+        Places.initialize(getContext(), "AIzaSyAejI8898winqzlekeYkhyJ2m1ZEPb3im0");
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG));
 
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                text.setText(place.getAddress());
+                address = place.getAddress();
+            }
+
+            @Override
+            public void onError(Status status) {
+                Toasty.error(getContext(),"Error : We can't use this address",Toasty.LENGTH_LONG).show();
+            }
+        });
+
+        Button positionButton = view.findViewById(R.id.AddManga_MyPositionButton);
+        positionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission();
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    Toasty.error(getContext(),"Error : Permisssion Denied",Toasty.LENGTH_LONG).show();
+                    return;
+                }
+                client.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        try {
+                            String s = getAddressFromLatLng(location.getLatitude(),location.getLongitude());
+                            text.setText(s);
+                            address = s;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                client.getLastLocation().addOnFailureListener(getActivity(), new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toasty.error(getContext(),"Error : We can't find your position",Toasty.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            private void requestPermission() {
+                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+        });
 
         Button button =  view.findViewById(R.id.AddFragementButton);
         button.setOnClickListener(new View.OnClickListener() {
@@ -53,13 +128,11 @@ public class AddMangaFragment extends Fragment {
                 EditText mangaName = view.findViewById(R.id.AddMangaName);
                 EditText price = view.findViewById(R.id.AddMangaPrice);
                 EditText sellerName = view.findViewById(R.id.AddMangaSellerName);
-                EditText address = view.findViewById(R.id.AddMangaAddres);
                 EditText telephone = view.findViewById(R.id.AddMangaTelephone);
 
                 String mangaNameString = mangaName.getText().toString();
                 String priceString = price.getText().toString();
                 String sellerNameString = sellerName.getText().toString();
-                String addressString = address.getText().toString();
                 String telephoneString = telephone.getText().toString();
 
                 RequestQueue queue = Volley.newRequestQueue(getContext());
@@ -71,7 +144,7 @@ public class AddMangaFragment extends Fragment {
                     jsonBody.put("price", priceString);
                     jsonBody.put("sellerName", sellerNameString);
                     jsonBody.put("telephone", telephoneString);
-                    jsonBody.put("adresse", addressString);
+                    jsonBody.put("adresse", address);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -95,5 +168,11 @@ public class AddMangaFragment extends Fragment {
         });
         return view;
     }
-
+    private String getAddressFromLatLng(double lat,double lng) throws IOException {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(getContext(), Locale.getDefault());
+        addresses = geocoder.getFromLocation(lat, lng, 1);
+        return addresses.get(0).getAddressLine(0);
+    }
  }
--- app/src/main/java/com/android/shopmanga/SearchMangaFragment.java
+++ app/src/main/java/com/android/shopmanga/SearchMangaFragment.java
@@ -1,12 +1,17 @@
 package com.android.shopmanga;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +19,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,11 +39,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Locale;
+
+import es.dmoral.toasty.Toasty;
 
 
 public class SearchMangaFragment extends Fragment {
 
-    String addressetext, mangaNametext;
+    private FusedLocationProviderClient client;
+
+    String mangaNametext;
+    double lat, lng;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,29 +60,85 @@ public class SearchMangaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_search_manga, container, false);
-        Button button =  view.findViewById(R.id.Searchbutton);
+        View view = inflater.inflate(R.layout.fragment_search_manga, container, false);
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+        Button button = view.findViewById(R.id.Searchbutton);
+        Button myPositionButton = view.findViewById(R.id.MyPositionButton);
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        EditText text = autocompleteFragment.getView().findViewById(R.id.places_autocomplete_search_input);
+        autocompleteFragment.getView().findViewById(R.id.places_autocomplete_search_button).setVisibility(View.GONE);
+
+        Places.initialize(getContext(), "AIzaSyAejI8898winqzlekeYkhyJ2m1ZEPb3im0");
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                text.setText(place.getAddress());
+                lat = place.getLatLng().latitude;
+                lng = place.getLatLng().longitude;
+            }
+
+            @Override
+            public void onError(Status status) {
+                Toasty.error(getContext(),"Error : can't choice this place",Toasty.LENGTH_LONG).show();
+            }
+        });
+
+        myPositionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission();
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    Toasty.error(getContext(),"Error : Permisssion Denied",Toasty.LENGTH_LONG).show();
+                    return;
+                }
+                client.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        text.setText("My position");
+                        lat = location.getLatitude();
+                        lng = location.getLongitude();
+                    }
+                });
+                client.getLastLocation().addOnFailureListener(getActivity(), new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toasty.error(getContext(),"Error : We can't find your position",Toasty.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Geocoder geo = new Geocoder(getContext());
-                double lat = 0,lng = 0;
 
-                EditText addresse = getView().findViewById(R.id.autoCompleteTextView);
                 EditText mangaName = getView().findViewById(R.id.MangaName);
-
-                addressetext = addresse.getText().toString();
                 mangaNametext = mangaName.getText().toString();
 
-                LatLngTask myTask = new LatLngTask();  //can pass other variables as needed
-                myTask.execute(addressetext,mangaNametext);
+                //LatLngTask myTask = new LatLngTask();  //can pass other variables as needed
+                //myTask.execute(addressetext,mangaNametext);
+
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                intent.putExtra("lat",lat);
+                intent.putExtra("lng",lng);
+                intent.putExtra("mangaName",mangaNametext);
+                startActivity(intent);
+
+
             }
         });
         return view;
     }
 
-    public class LatLngTask extends AsyncTask<String, Void , String> {
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+    }
+   /* public class LatLngTask extends AsyncTask<String, Void , String> {
 
         @Override
         protected String doInBackground(String... text) {
@@ -122,6 +200,6 @@ public class SearchMangaFragment extends Fragment {
                 e.printStackTrace();
             }
         }
-    }
+    }*/
 
 }
