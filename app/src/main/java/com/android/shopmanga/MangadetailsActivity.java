f83f600eabafb6ed84d2e9942a326c4aab09e01d
hatouxx <hatouxx@gmail.com>
2019-04-01 20:10:55 +0200

    fonctionnalite bouton_call

--- app/src/main/java/com/android/shopmanga/MangadetailsActivity.java
+++ app/src/main/java/com/android/shopmanga/MangadetailsActivity.java
@@ -1,10 +1,19 @@
 package com.android.shopmanga;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MangadetailsActivity extends AppCompatActivity {
+
+
+    TelephonyManager telephonyManager; // pour recuperer l'etat de l'application 'telephone'
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +28,24 @@ public class MangadetailsActivity extends AppCompatActivity {
         TextView address = findViewById(R.id.address);
         TextView telephone = findViewById(R.id.telephone);
 
-        mangaName.setText( "Manga name :  "+manga.getMangaName());
-        price.setText(     "Price :       "+Double.toString(manga.getPrice()));
-        sellerName.setText("Seller Name : "+manga.getSellerName());
-        address.setText(   "Addresse :    "+manga.getAddress());
-        telephone.setText( "Telephone :   "+manga.getTelephone());
+        mangaName.setText( "Manga name :  " + manga.getMangaName());
+        price.setText(     "Price :       " + Double.toString(manga.getPrice()));
+        sellerName.setText("Seller Name : " + manga.getSellerName());
+        address.setText(   "Addresse :    " + manga.getAddress());
+        telephone.setText( "Telephone :   " + manga.getTelephone());
+
+
+        // ici on gere le bouton call
+        ImageView callButton = findViewById(R.id.call_button);
+        telephonyManager = (TelephonyManager) this.getSystemService(this.TELEPHONY_SERVICE);
+        callButton.setOnClickListener(v -> {
+            if(telephonyManager.getCallState() == TelephonyManager.CALL_STATE_IDLE) {
+                Intent appel = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + telephone.getText()));
+                startActivity(appel);
+            }
+        });
+
+
+
     }
 }
