0b5111e6c2c2eab66c8b16c7117a0d372b099348
Ayoub Mouhri <ayoublaw123@gmail.com>
2019-04-04 14:53:48 +0200

    Merge with call_button

--- app/src/main/java/com/android/shopmanga/MangadetailsActivity.java
+++ app/src/main/java/com/android/shopmanga/MangadetailsActivity.java
@@ -1,15 +1,19 @@
 package com.android.shopmanga;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Picture;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -18,6 +22,8 @@ import java.net.URL;
 public class MangadetailsActivity extends AppCompatActivity {
 
     ImageView image;
+    TelephonyManager telephonyManager; // pour recuperer l'etat de l'application 'telephone'
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +44,21 @@ public class MangadetailsActivity extends AppCompatActivity {
         sellerName.setText("Seller Name : "+manga.getSellerName());
         address.setText(   "Addresse :    "+manga.getAddress());
         telephone.setText( "Telephone :   "+manga.getTelephone());
+
+        // ici on gere le bouton call
+        ImageView callButton = findViewById(R.id.call_button);
+        telephonyManager = (TelephonyManager) this.getSystemService(this.TELEPHONY_SERVICE);
+        callButton.setOnClickListener(v -> {
+            if(telephonyManager.getCallState() == TelephonyManager.CALL_STATE_IDLE) {
+                Intent appel = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + telephone.getText()));
+                startActivity(appel);
+            }
+        });
     }
     private void LoadImageFromUrl(String url){
         Picasso.with(this).load(url)
                 .error(R.mipmap.ic_launcher)
+                .resize(720,720)
                 .into(image, new Callback() {
                     @Override
                     public void onSuccess() {
